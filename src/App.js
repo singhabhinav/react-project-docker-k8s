ef3884ca5301e91aae05453d3769b76c0e0dab4f
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-22 15:00:18 +0000

    Sample commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Abhinav & Devops Course Team.
+          Edit <code>src/App.js</code> and save to reload by Abhinav Singh & Devops Course Team.
         </p>
         <a
           className="App-link"
