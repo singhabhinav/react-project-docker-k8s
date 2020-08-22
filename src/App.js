ff976fe6ce6a439f1b0d8502119e630877add1c3
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-22 15:22:13 +0000

    Added one more paragraph

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload by Abhinav Singh & Devops Course Team.
         </p>
+        <p>
+            Added one more line
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
