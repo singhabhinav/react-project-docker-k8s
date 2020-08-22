5862429d478ee06e7a29ca4d86d9c0d61a044850
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-22 16:37:17 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload by Abhinav Singh & Devops Course Team.
         </p>
         <p>
-            Added one more line
+            Added one more line on master branch
         </p>
         <a
           className="App-link"
