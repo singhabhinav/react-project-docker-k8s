3e85448aabdcf3a9fc8d58481f9802734b40ce4e
Abhinav Singh <abhinav.foss@gmail.com>
2020-10-03 14:41:48 +0000

    Added petyy changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Abhinav Singh & Devops Course Team.
+          Edit <code>src/App.js</code> and save to reload by Abhinav Singh V2.
         </p>
         <p>
             I am on signup branch
